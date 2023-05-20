a92f8fc19b42e46ee895947fcd8f239cc4c281a2
L17E <l.yte@yandex.ru>
2023-05-20 15:23:44 +0300

    lsp auto-format on save

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,6 +1,7 @@
 local autocmd = vim.api.nvim_create_autocmd
 
-vim.api.nvim_set_option('relativenumber', true)
+vim.api.nvim_set_option("relativenumber", true)            -- set rnu
+vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]] -- auto-format on save
 
 autocmd("vimresized", {
   pattern = "*",
