d52d18aab452784df0d4187e50a0c06294b989a0
L17E <l.yte@yandex.ru>
2023-05-20 11:50:12 +0300

    prettier & auto 'relative number'

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,7 +1,8 @@
--- local autocmd = vim.api.nvim_create_autocmd
+local autocmd = vim.api.nvim_create_autocmd
 
--- Auto resize panes when resizing nvim window
--- autocmd("VimResized", {
---   pattern = "*",
---   command = "tabdo wincmd =",
--- })
+vim.api.nvim_set_option('relativenumber', true)
+
+autocmd("vimresized", {
+  pattern = "*",
+  command = "tabdo wincmd =",
+})
